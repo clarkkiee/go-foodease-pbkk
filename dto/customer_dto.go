20b5adfd75469e30e274fc2b4275af072c5d7a6c
clarkkkkk <juanfarrel0404@gmail.com>
2024-11-20 00:49:49 +0700

    feat: add customer register dto

--- dto/customer_dto.go
+++ dto/customer_dto.go
@@ -20,4 +20,12 @@ type (
 		Token string `json:"token"`
 		ID string `json:"id"`
 	}
+
+	CustomerRegisterRequest struct {
+		Email string `json:"email" form:"email" binding:"required"`
+		FirstName string `json:"first_name" form:"first_name" binding:"required"`
+		LastName string `json:"last_name" form:"last_name" binding:"required"`
+		Password string `json:"password" form:"password" binding:"required"`
+		ActiveAddressId *uuid.UUID `json:"active_address_id"`
+	}
 )
