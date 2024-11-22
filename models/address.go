d82e1e9c73df96a14e582e0fca4c1adf9f42e5c0
clarkkkkk <juanfarrel0404@gmail.com>
2024-11-22 15:22:51 +0700

    feat: define address models and its relation

---
+++ models/address.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"go-foodease-be/types"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Address struct {
+	ID uuid.UUID `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()" json:"id"`
+    Street string `json:"street"`
+    Coordinates types.Coordinates `gorm:"type:geometry(Point,4326)" json:"coordinates"`
+    CustomerID  uuid.UUID  `gorm:"type:uuid;not null" json:"customer_id"`
+	Customer    Customer   `gorm:"foreignKey:CustomerID" json:"-"`
+    CreatedAt time.Time `json:"created_at"`
+    UpdatedAt time.Time `json:"updated_at"`
+}
