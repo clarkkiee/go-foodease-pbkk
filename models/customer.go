511cf060d06b70dafba5f0df229cdf468e9878bf
clarkkkkk <juanfarrel0404@gmail.com>
2024-11-23 00:56:56 +0700

    fix: customer-address relation and index attr

--- models/customer.go
+++ models/customer.go
@@ -10,11 +10,12 @@ import (
 
 type Customer struct {
 	ID             uuid.UUID  `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()" json:"id"`
-	Email          string     `json:"email"`
+	Email          string     `gorm:"uniqueIndex:idx_customer_email" json:"email"`
 	FirstName      string     `json:"first_name"`
 	LastName       string     `json:"last_name"`
 	Password       string     `gorm:"column:customer_password" json:"password"`
 	ActiveAddressId *uuid.UUID `gorm:"type:uuid;default:null" json:"active_address_id"`
+	Addresses       []Address  `gorm:"foreignKey:CustomerID" json:"addresses"`
 	CreatedAt      time.Time  `json:"created_at"`
 	UpdatedAt      time.Time  `json:"updated_at"`
 }
