3a2f77c10e2d0914fa279155ea8083353b9eb482
Etpaxtysus <itsrhenaldy@gmail.com>
2024-11-28 23:09:59 +0700

    feat: add product dto

---
+++ dto/product_dto.go
@@ -0,0 +1,30 @@
+package dto
+
+import "time"
+
+type CreateProductRequest struct {
+	ProductName   string    `json:"product_name"`
+	Description   string    `json:"description"`
+	PriceBefore   uint64    `json:"price_before"`
+	PriceAfter    uint64    `json:"price_after"`
+	ProductionTime time.Time `json:"production_time"`
+	ExpiredTime   time.Time `json:"expired_time"`
+	Stock         uint64    `json:"stock"`
+	CategoryID    string    `json:"category_id"`
+	ImageID       *string   `json:"image_id"`
+}
+
+type ProductResponse struct {
+	ID            string    `json:"id"`
+	ProductName   string    `json:"product_name"`
+	Description   string    `json:"description"`
+	PriceBefore   uint64    `json:"price_before"`
+	PriceAfter    uint64    `json:"price_after"`
+	ProductionTime time.Time `json:"production_time"`
+	ExpiredTime   time.Time `json:"expired_time"`
+	Stock         uint64    `json:"stock"`
+	CategoryID    string    `json:"category_id"`
+	ImageID       *string   `json:"image_id"`
+	CreatedAt     time.Time `json:"created_at"`
+	UpdatedAt     time.Time `json:"updated_at"`
+}
