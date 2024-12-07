bf8524ff2a35aeeb1abcb655a5f2a15072d6c3f9
Aloysius Juan Farrel Lumentut <82431271+clarkkiee@users.noreply.github.com>
2024-12-07 23:42:15 +0700

    resolve conflict

--- repository/product_repository.go
+++ repository/product_repository.go
@@ -80,6 +80,26 @@ func (r *productRepository) UpdateProduct(ctx context.Context, productID string,
 	return res, nil
 }
 
+func (r *productRepository) GetMinimumProduct(ctx context.Context, tx *gorm.DB, productId string) (dto.GetMinimumProductResult, error) {
+	if tx == nil {
+		tx = r.db
+	}
+
+	var product models.Product
+	if err := tx.WithContext(ctx).Model(&models.Product{}).Select("id, stock, store_id, category_id").Where("id = ?", productId).Take(&product).Error; err != nil {
+		return dto.GetMinimumProductResult{}, err
+	}
+
+	res := dto.GetMinimumProductResult{
+		ID: product.ID.String(),
+		Stock: product.Stock,
+		StoreID: product.StoreId.String(),
+		CategoryID: product.CategoryID.String(),
+	}
+
+	return res, nil
+}
+
 func (r *productRepository) DeleteProduct(ctx context.Context, productID string, storeID string) error {
     var product models.Product
     if err := r.db.WithContext(ctx).Where("id = ? AND store_id = ?", productID, storeID).First(&product).Error; err != nil {
