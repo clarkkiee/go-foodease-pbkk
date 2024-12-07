b3a8a07a739bb4c01aae7a6278cda4b01cfac1f1
igabakarayam <ignatiusidabagusa@gmail.com>
2024-12-07 22:18:48 +0700

    add delete product need fix

--- routes/product_route.go
+++ routes/product_route.go
@@ -4,15 +4,18 @@ import (
 	"go-foodease-be/controller"
 	"go-foodease-be/middleware"
 	"go-foodease-be/service"
-
 	"github.com/gin-gonic/gin"
 )
+
 
 func Product(route *gin.Engine, productController controller.ProductController, jwtService service.JWTService) {
     routes := route.Group("/api/product")
     {
-        routes.POST("/create", middleware.Authenticate(jwtService), productController.CreateProduct)
-        routes.PUT("/update/:product_id", middleware.Authenticate(jwtService), productController.UpdateProduct) 
-    }
+        
+        //routes.POST("/create", middleware.Authenticate(jwtService), productController.CreateProduct)
+        routes.DELETE("/delete/:product_id", middleware.Authenticate(jwtService), productController.DeleteProduct) 
+        //routes.PUT("/update/:product_id", middleware.Authenticate(jwtService), productController.UpdateProduct)
+
 }
 
+}
